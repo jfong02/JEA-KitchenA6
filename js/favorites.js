f27aa0538a7c7ada49ef48d303bf58dd11c0b855
jfong02 <jfong02@users.noreply.github.com>
2018-11-08 22:19:48 -0800

    handlebars, local storage testing

--- js/favorites.js
+++ js/favorites.js
@@ -20,3 +20,37 @@ function closeNav() {
     $("#opensidebar").show();
     $("#closesidebar").hide();
 }
+
+var favorited = [
+    {'name':'chicken_kale_soup_recipe','index':'1'},
+    {'name':'cranberry_kale_salad_recipe','index':'2'}
+]
+var dummy = [
+    {'name':'Recipe Name','index':'#'}
+]
+
+// var source = $("#recipetemplate").html();
+// var template = Handlebars.compile(source);
+// var parent = $("#templatedfavorites");
+
+// var html = template(simple)
+// for (var i = 0; i<favorited.length; i++) {
+//     var curdata = favorited[i];
+//     var curhtml = template(curdata);
+//     parent.append(curhtml);
+// }
+localStorage.setItem('favoritedrecipes',JSON.stringify(dummy));
+localStorage.setItem('favoritedrecipes',JSON.stringify(favorited));
+var favorites = localStorage.getItem('favoritedrecipes',favorited);
+if (favorites) {
+    var source = $("#recipetemplate").html(favorites);
+    var template = Handlebars.compile(source);
+    var parent = $("#templatedfavorites");
+
+    var html = template(dummy)
+    for (var i = 0; i<favorites.length; i++) {
+        var curdata = favorites[i];
+        var curhtml = template(curdata);
+        parent.append(curhtml);
+    }
+}
