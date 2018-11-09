a537f8ce40491dcae955c89d4f8e7e386d618229
jfong02 <jfong02@users.noreply.github.com>
2018-11-08 18:53:29 -0800

    navbar changed to collapsible sidenav

--- js/homepage.js
+++ js/homepage.js
@@ -2,12 +2,27 @@ $(document).ready(function() {
     
     
     // Navbar Active Selection
-    $('.nav-item').click(function()
-    {
-      $('navbar a.nav-item').find(".active").removeClass(" active");
-      $(this).parent().addClass(" active");
-      setNavigation();
-      console.log("Figure out how to change active class")
-    });
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+          return this.href == url;
+    }).parent().addClass('active');
 
+    
 });
+
+// * Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mainnav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    $("#opensidebar").hide();
+    $("#closesidebar").show();
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mainnav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    $("#opensidebar").show();
+    $("#closesidebar").hide();
+}
